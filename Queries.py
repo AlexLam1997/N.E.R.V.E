d2a6c904f1b780e21d29f17b7ab0ef8e1f77b64b
AlexLam1997 <alexander.lam@Mail.mcgill.ca>
2018-12-01 13:55:56 -0500

    finished getting urls from database

--- Queries.py
+++ Queries.py
@@ -1,12 +1,15 @@
 import requests
+import ssl
+import config
 
 URL = "https://api.iextrading.com/1.0"
 
-r = requests.get(url = URL + "/stock/aapl/news/last/1")
+equitiesList = ["aapl", "googl", "fb", "msft", "gs", "ge"]
 
+r = requests.get(url = URL + "/stock/aapl/news/last/5")
 data = r.json()
 
-newUrl = data['url'][0]
+for number in range(len(data)):
+    config.queue.put(data[number]['url'])
 
-print(newsUrl)
 
